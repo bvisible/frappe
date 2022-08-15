aeb0099efe3b04d997592a51787c5e9eced3d0d7
bvisible <info@bvisible.ch>
2022-08-15 17:21:40 +0200

    Update grid_row_form.js

--- frappe/public/js/frappe/form/grid_row_form.js
+++ frappe/public/js/frappe/form/grid_row_form.js
@@ -78,7 +78,8 @@ export default class GridRowForm {
 			</div>`;
 
 			$(template).appendTo(this.wrapper);
-			this.form_area = this.wrapper.find(".form-area");
+			////this.form_area = this.wrapper.find(".form-area");
+			this.form_area = this.wrapper.find(".form-area").addClass(this.row.doc.item_name);
 			this.row.set_row_index();
 			this.set_form_events();
 		}
