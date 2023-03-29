0af390de130e18446b538ac3298a736945a891b4
bVisible <info@bvisible.ch>
2023-03-29 19:14:23 +0200

    reduct time for progress bar

--- frappe/templates/includes/login/login.js
+++ frappe/templates/includes/login/login.js
@@ -271,7 +271,7 @@ login.login_handlers = (function () {
 							}
 					}, 10);
 				}
-				startLoadingBar(15);
+				startLoadingBar(10);
 				////
 			} else if (data.message == 'Password Reset') {
 				window.location.href = frappe.utils.sanitise_redirect(data.redirect_to);
