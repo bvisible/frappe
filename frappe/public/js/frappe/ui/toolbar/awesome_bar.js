fcd1372a1ef471a5bbbf68b5acdd3ac68b17eee0
Jérémy Christillin <jeremy@neoservice.ai>
2026-04-07 12:16:16 +0200

    fix(search): don't close search panel when interacting with preview dialog

--- frappe/public/js/frappe/ui/toolbar/awesome_bar.js
+++ frappe/public/js/frappe/ui/toolbar/awesome_bar.js
@@ -156,11 +156,13 @@ frappe.search.AwesomeBar = class AwesomeBar {
 
 		this.$input.on("keydown", (e) => this._handle_keydown(e));
 
-		// Close on outside click
+		// Close on outside click (but not when clicking on modals/dialogs)
 		$(document).on("mousedown", (e) => {
 			if (
 				!$(e.target).closest(".search-bar").length &&
-				!$(e.target).closest(".search-mega-panel").length
+				!$(e.target).closest(".search-mega-panel").length &&
+				!$(e.target).closest(".modal").length &&
+				!$(e.target).closest(".modal-backdrop").length
 			) {
 				this._close();
 			}
